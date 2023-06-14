d68a46d99f03be81a7343f3d20f38b898012cadf
harirathod <h4ri.rathod@gmail.com>
2023-06-14 17:46:21 +0100

    Rename file

---
+++ src/main/java/com/web/webapp/WebApplication.java
@@ -0,0 +1,24 @@
+package com.web.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * The main Spring Boot class.
+ */
+@RestController
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+	@GetMapping("/gre")
+	public void error() {
+		throw new IllegalArgumentException();
+	}
+
+}
