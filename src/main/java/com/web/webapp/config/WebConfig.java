e6d83d56cc62e0357329e9dd91fc63b02c8bb64e
harirathod <h4ri.rathod@gmail.com>
2023-06-19 21:16:29 +0100

    Update class documentation

--- src/main/java/com/web/webapp/config/WebConfig.java
+++ src/main/java/com/web/webapp/config/WebConfig.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+/**
+ * Configuration class containing configuration beans. Ensures the application has access to required resources.
+ * @version 2023.06.19
+ * @author hari_rathod
+ */
 @Configuration
 public class WebConfig
 {
