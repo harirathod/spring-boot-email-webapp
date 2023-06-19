093d185a9dde0fee1bf9f36523451df7c85340c2
harirathod <h4ri.rathod@gmail.com>
2023-06-19 12:21:53 +0100

    Add fields and methods for host, port, etc.

--- src/main/java/com/web/webapp/Email.java
+++ src/main/java/com/web/webapp/Email.java
@@ -1,10 +1,14 @@
 package com.web.webapp;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-
+/**
+ * Class defining an Email object.
+ * Provides methods for accessing and modifying host, port, sender, recipient, content, subject, username, and password.
+ * @version 2023.06.19
+ * @author hari_rathod
+ */
 public class Email
 {
     // We have to use the full path name for @Email, as the containing class is also called Email.
@@ -16,8 +20,24 @@ public class Email
     @jakarta.validation.constraints.Email(message = "Must be an email.")
     private String to;
 
+    @NotBlank(message = "Subject of the email cannot be empty.")
+    @Length(max = 78)
+    private String subject;
+
     @NotBlank(message = "Content of the email cannot be empty.")
     private String content;
+
+    @NotBlank(message = "Host must be provided.")
+    private String host;
+
+    @NotBlank(message = "Port must be provided.")
+    private String port;
+
+    @NotBlank(message = "Username is required for validation.")
+    private String username;
+
+    @NotBlank(message = "Password is required for validation.")
+    private String password;
 
     public String getFrom()
     {
@@ -29,9 +49,34 @@ public class Email
         return to;
     }
 
+    public String getSubject()
+    {
+        return subject;
+    }
+
     public String getContent()
     {
         return content;
+    }
+
+    public String getHost()
+    {
+        return host;
+    }
+
+    public String getPort()
+    {
+        return port;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
     }
 
     public void setTo(String to)
@@ -44,9 +89,34 @@ public class Email
         this.from = from;
     }
 
+    public void setSubject(String subject)
+    {
+        this.subject = subject;
+    }
+
     public void setContent(String content)
     {
         this.content = content;
+    }
+
+    public void setHost(String host)
+    {
+        this.host = host;
+    }
+
+    public void setPort(String port)
+    {
+        this.port = port;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
     }
 
     public String toString()
