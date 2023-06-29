d355d86676b754cfda8c90e7ae3e8530e0281511
harirathod <h4ri.rathod@gmail.com>
2023-06-29 08:54:40 +0100

    Add feature so recent emails are displayed

--- src/main/resources/static/email.js
+++ src/main/resources/static/email.js
@@ -14,19 +14,40 @@ function getRecentEmails(callback) {
 
 function displayInDocument(arrayOfEmails, nameOfHtmlContainer) {
     var container = document.querySelector(nameOfHtmlContainer);
+    container.replaceChildren();
+    if (arrayOfEmails.length == 0) {
+        var none = document.createElement('div');
+        none.textContent = `No emails to display.`
+        container.appendChild(none);
+        return;
+    }
     for (i = 0; i < arrayOfEmails.length; i++) {
         var email = document.createElement('div');
-        email.textContent = `Sender: ${arrayOfEmails[i].sender}, Recipient: ${arrayOfEmails[i].recipient},\n Content: ${arrayOfEmails[i].content}`;
+        email.className = "email-ctnr";
+
+        var container1 = document.createElement('div');
+        container1.className = "sender-recipient-ctnr";
+        var from = document.createElement('div');
+        var arrow = document.createElement('span');
+        var to = document.createElement('div');
+
+        from.textContent = `${arrayOfEmails[i].sender}`;
+        to.textContent = `${arrayOfEmails[i].recipient}`;
+        arrow.textContent = "==>"
+        container1.replaceChildren(from, arrow, to);
+
+        var container2 = document.createElement('div');
+        container2.className = "content-ctnr";
+        container2.textContent = `\"${arrayOfEmails[i].content}\"`;
+
+        email.replaceChildren(container1, container2);
         container.appendChild(email);
-        console.log(container);
-        console.log(email);
     }
 }
-
 
 var btn = document.querySelector('.view-recent-btn');
 btn.addEventListener('click', (e) => {
     getRecentEmails(function(data) {
-        displayInDocument(data, ".recent-emails-ctnr")
+        displayInDocument(data, ".recent-emails-ctnr");
     });
 });
