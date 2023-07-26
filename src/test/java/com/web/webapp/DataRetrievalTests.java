bbcb324cbf1665c038af7961f0ee78d5a4ad705a
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-07-26 13:13:24 +0100

    Add privacy tests with h2 in-memory database

--- src/test/java/com/web/webapp/DataRetrievalTests.java
+++ src/test/java/com/web/webapp/DataRetrievalTests.java
@@ -1,23 +1,47 @@
 package com.web.webapp;
 
 import com.web.webapp.config.WebConfig;
+import com.web.webapp.repository.EmailRepository;
 import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import({ WebConfig.class, EmailController.class })
+@Import({ WebConfig.class })
 public class DataRetrievalTests
 {
     @Autowired
-    private EmailController emailController;
+    private EmailRepository emailRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     @Test
     public void testPrivacyOfCredentials() throws MessagingException {
+        Email email = new Email();
+        email.setUsername("my_username")
+                .setPassword("secret_password")
+                .setContent("content")
+                .setRecipient("r@r.com")
+                .setHost("smtp.unknown.com")
+                .setPort("000")
+                .setSender("s@s.com")
+                .setSubject("subject");
 
+        emailRepository.save(email);
+        testEntityManager.flush();
+        testEntityManager.clear();
+        Email retrievedEmail = emailRepository.findAll().get(0);
+
+        assertNotNull(retrievedEmail);
+        assertNull(retrievedEmail.getPassword());
+        assertNull(retrievedEmail.getUsername());
     }
 }
