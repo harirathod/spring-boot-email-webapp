7bced3d599dcbcc4c090f40346f8bef18777fb7f
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-07-26 12:33:58 +0100

    Add @PropertySource back

--- src/main/java/com/web/webapp/config/WebConfig.java
+++ src/main/java/com/web/webapp/config/WebConfig.java
@@ -1,11 +1,7 @@
 package com.web.webapp.config;
 
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
@@ -14,16 +10,16 @@ import org.springframework.context.support.ResourceBundleMessageSource;
  * @author hari_rathod
  */
 @Configuration
-public class WebConfig
-{
+@PropertySource("classpath:database.properties")
+public class WebConfig {
     /**
      * This Configuration Bean identifies the properties file, allowed the messages inside the
      * properties file to be accessed by Thymeleaf.
+     *
      * @return The Bean that identifies the application.properties file.
      */
     @Bean
-    public ResourceBundleMessageSource messageSource()
-    {
+    public ResourceBundleMessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("application");
         return messageSource;
