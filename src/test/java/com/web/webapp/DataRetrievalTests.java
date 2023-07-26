f05781341b52ae53199f9520973bc248d4f7c2c5
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-07-26 12:33:43 +0100

    Configure class with in-memory H2 database

--- src/test/java/com/web/webapp/DataRetrievalTests.java
+++ src/test/java/com/web/webapp/DataRetrievalTests.java
@@ -1,9 +1,23 @@
 package com.web.webapp;
 
+import com.web.webapp.config.WebConfig;
+import jakarta.mail.MessagingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({ WebConfig.class, EmailController.class })
 public class DataRetrievalTests
 {
+    @Autowired
+    private EmailController emailController;
 
+    @Test
+    public void testPrivacyOfCredentials() throws MessagingException {
+
+    }
 }
